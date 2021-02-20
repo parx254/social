6afb1527367c8c1b2da20b184f75fd10ae5e043f
Parker <49407804+parx254@users.noreply.github.com>
2021-02-20 18:04:02 -0500

    Update header.php

--- pages/header.php
+++ pages/header.php
@@ -41,7 +41,7 @@
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <meta http-equiv="X-UA-Compatible" content="ie=edge">
       <title><?php echo $title; ?></title>
-      <link rel="stylesheet" href="/css/style.css" media="screen">
+      <link rel="stylesheet" href="/css/style.php" media="screen">
       <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.7.0/css/all.css'>
       <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
