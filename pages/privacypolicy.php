b45b40f444a01079ea9dd6e9d07f4ebb57261520
Parker <49407804+parx254@users.noreply.github.com>
2021-02-21 01:38:11 -0500

    Update privacypolicy.php

--- pages/privacypolicy.php
+++ pages/privacypolicy.php
@@ -1,49 +1,14 @@
 <?php
   include('functions.php');
+  $title = "Privacy Policy | Social Destinations";      // (1) Set the title
+  $description = 'Privacy Policy';  // (2) Set the description
+  $city = 'Paris';                  // (3) Set the city
+  $keywords = 'Privacy Policy, Travel, Trips, Travel Tips, Excursions, Sports, Holidays, Social Destinations, Social, Destinations';          // Set the keywords
+  $activePage = 'none';             // (4) Set the active page
+  $currentcolor = '#000';           // (5) Set the color
+  include "header.php";                // (6) Include the header
   ?>
-<html lang="en">
-  <head>
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-131583482-3"></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', 'UA-131583482-3');
-    </script>
-    <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png">
-    <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png">
-    <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png">
-    <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png">
-    <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png">
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png">
-    <link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-    <link rel="manifest" href="/manifest.json">
-    <meta name="msapplication-TileColor" content="#ffffff">
-    <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
-    <meta name="theme-color" content="#ffffff">
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Privacy Policy | Social Destinations</title>
-    <link rel="stylesheet" href="/css/main.css">
-    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'>
-    <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
-    <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,700,900" rel="stylesheet">
-    <script src="/js/myscript.js"></script> 
-    <style>
-      @media(max-width:900px){nav{border-bottom:1px solid #f5f5f5;height:50px;position:static;z-index:1;top:0}#toggle .span{background:#000}}
-      @media(min-width:901px){nav{background: #000;position:static}}
-    </style>
-  </head>
-  <?php nav(); ?>
-  <body>
+  <body> 
     <div class="bodycontainer">
       <div class="maintitle">
         <h1>Privacy Policy</h1>
@@ -105,5 +70,9 @@
     include "footer.php";
     ?>
     </body>
-  <script src="/js/script.js"></script> 
+      <style>
+      @media(max-width:900px){nav{border-bottom:1px solid #f5f5f5;height:50px;position:static;z-index:1;top:0}#toggle .span{background:#000}}
+      @media(min-width:901px){nav{background: #000;position:absolute}
+      .maintitle{margin-top:100px}
+    </style>
 </html>
