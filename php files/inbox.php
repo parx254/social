cb214aac58e1ce0042ff59e6c077591d9faedaa2
Parker <49407804+parx254@users.noreply.github.com>
2025-11-11 17:16:59 -0700

    Update inbox.php

--- php files/inbox.php
+++ php files/inbox.php
@@ -1,6 +1,6 @@
 <?php
-require_once 'control.php';
 require_once 'functions.php';
+require_once 'control.php';
 verifySessionAndUser();
 $title = "Inbox | Social Destinations";
 $city = 'Nashville';
