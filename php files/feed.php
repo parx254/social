f11c0cab7f24d32709a08f7426fc0a71bb1a9f0a
Parker <49407804+parx254@users.noreply.github.com>
2025-11-11 17:17:55 -0700

    Fix require_once order in feed.php

--- php files/feed.php
+++ php files/feed.php
@@ -1,6 +1,6 @@
 <?php
-require_once 'control.php';
 require_once 'functions.php';
+require_once 'control.php';
 verifySessionAndUser();
 $title = "Feed | Social Destinations";
 $activePage = 'feed';
